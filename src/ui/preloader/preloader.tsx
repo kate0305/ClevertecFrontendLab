da5ba05d94b02edc06338f9f9b7d9bb1191a824d
Katsiaryna Voronich <katarinka403@gmail.com>
2023-02-17 10:38:43 +0300

    fix: fix test-id spelling

--- src/ui/preloader/preloader.tsx
+++ src/ui/preloader/preloader.tsx
@@ -2,7 +2,7 @@ import classes from './preloader.module.css';
 
 
 export const Preloader = () => (
-  <div className={classes.wrapper} data-testid='loader'>
+  <div className={classes.wrapper} data-test-id='loader'>
     <div className={classes.preloader} />
   </div>
 );
