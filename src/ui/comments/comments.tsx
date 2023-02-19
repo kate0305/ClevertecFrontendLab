d6749ad46ec73706f55e8e6d17544c193fed9789
Katsiaryna Voronich <katarinka403@gmail.com>
2023-02-19 17:57:44 +0300

    refactor: add comment component

--- src/ui/comments/comments.tsx
+++ src/ui/comments/comments.tsx
@@ -1,69 +1,37 @@
 import { useState } from 'react';
 
-import userFoto from '../../assets/images/jpeg/user-review.jpg';
-import { Button } from '../buttons/btn';
+import { CommentData } from '../../utils/types/book';
 import { ButtonDropdown } from '../buttons/btn-dropdown';
+import { Button } from '../buttons/btn-primary';
+
+import { Comment } from './comment';
 
 import classes from './comments.module.css';
 
-export const Comments = () => {
-    const [isOpen, setOpen] = useState<boolean>(false)
+type CommentsProps = {
+  comments: CommentData[] | null;
+};
 
-    const toggleReviews = () => setOpen(!isOpen);
+export const Comments = ({ comments }: CommentsProps) => {
+  const [isOpen, setOpen] = useState<boolean>(false);
 
-    return (
-      <div className={classes.reviews}>
-        <div className={classes.container}>
-          <h5 className={classes.title}>
-            Отзывы
-            <span className={classes.amount}>2</span>
-          </h5>
-          <ButtonDropdown isOpen={isOpen} onClick={toggleReviews} isColor={false} />
-        </div>
-        {isOpen && (
-          <div className={classes.reviewsWrapper}>
-            <div className={classes.review}>
-              <div className={classes.userData}>
-                <img src={userFoto} alt='user foto' />
-                <div className={classes.userDescription}>
-                  <p className={classes.name}>Иван Иванов</p>
-                  <p className={classes.date}>5 января 2019</p>
-                </div>
-              </div>
-              <div className={classes.reviewStars} />
-            </div>
-            <div className={classes.review}>
-              <div className={classes.userData}>
-                <img src={userFoto} alt='user foto' />
-                <div className={classes.userDescription}>
-                  <p className={classes.name}>Иван Иванов</p>
-                  <p className={classes.date}>5 января 2019</p>
-                </div>
-              </div>
-              <div className={classes.reviewStars} />
-              <p className={classes.reviewText}>
-                Учитывая ключевые сценарии поведения, курс на социально-ориентированный национальный проект не оставляет
-                шанса для анализа существующих паттернов поведения. Для современного мира внедрение современных методик
-                предоставляет широкие возможности для позиций, занимаемых участниками в отношении поставленных задач.
-                Как уже неоднократно упомянуто, сделанные на базе интернет-аналитики выводы будут в равной степени
-                предоставлены сами себе. Вот вам яркий пример современных тенденций — глубокий уровень погружения
-                создаёт предпосылки для своевременного выполнения сверхзадачи. И нет сомнений, что акционеры крупнейших
-                компаний, инициированные исключительно синтетически, превращены в посмешище, хотя само их существование
-                приносит несомненную пользу обществу.
-              </p>
-            </div>
-            <div style={{ paddingBottom: '0' }} className={classes.review}>
-              <div className={classes.userData}>
-                <img src={userFoto} alt='user foto' />
-                <div className={classes.userDescription}>
-                  <p className={classes.name}>Иван Иванов</p>
-                  <p className={classes.date}>5 января 2019</p>
-                </div>
-              </div>
-              <div className={classes.reviewStars} />
-            </div>
-          </div>
-        )}
-        <Button />
+  const toggleReviews = () => setOpen(!isOpen);
+
+  return (
+    <div className={classes.reviews}>
+      <div className={classes.container}>
+        <h5 className={classes.title}>
+          Отзывы
+          <span className={classes.amount}>{comments?.length}</span>
+        </h5>
+        <ButtonDropdown isOpen={isOpen} onClick={toggleReviews} isColor={false} />
       </div>
-    );};
+      {isOpen && (
+        <div className={classes.reviewsWrapper}>
+          {comments && comments.map((comment) => <Comment commentData={comment} key={comment.id} />)}
+        </div>
+      )}
+      <Button text='Оценить книгу' className='btnRate'/>
+    </div>
+  );
+};
