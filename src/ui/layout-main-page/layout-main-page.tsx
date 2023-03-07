3469d806d709cc84de24e3097bb2e67c711e9bce
Katsiaryna Voronich <katarinka403@gmail.com>
2023-03-07 13:51:42 +0300

    refactor: update constants

--- src/ui/layout-main-page/layout-main-page.tsx
+++ src/ui/layout-main-page/layout-main-page.tsx
@@ -6,13 +6,14 @@ import { NavBar } from '../nav-bar';
 import classes from './layout-main-page.module.css';
 
 export const LayoutMainPage = () => {
-    const isBurgerOpen = useAppSelector((state) => state.burgerReduser.isMenuOpen);
-    
-    return(
-  <main className={classes.wrapper}>
-    {isBurgerOpen || <NavBar />}
-    <div className={classes.content}>
-      <Outlet />
-    </div>
-  </main>
-)};
+  const isBurgerOpen = useAppSelector((state) => state.burgerReduser.isMenuOpen);
+
+  return (
+    <main className={classes.wrapper}>
+      {isBurgerOpen || <NavBar />}
+      <div className={classes.content}>
+        <Outlet />
+      </div>
+    </main>
+  );
+};
